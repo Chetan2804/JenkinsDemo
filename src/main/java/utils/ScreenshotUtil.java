71af370c6ded44cfa5e763cffa695bae477fed54
Chetan2804 <86292663+Chetan2804@users.noreply.github.com>
2026-04-14 16:10:48 +0530

    Changes done in file

---
+++ src/main/java/utils/ScreenshotUtil.java
@@ -0,0 +1,33 @@
+//package utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import org.apache.commons.io.FileUtils;
+
+public class ScreenshotUtil {
+
+    public static void capture(WebDriver driver, String fileName) {
+
+        try {
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+            File dest = new File("screenshots/" + fileName + "_" + timestamp + ".png");
+
+            FileUtils.copyFile(src, dest);
+
+            System.out.println("Screenshot saved: " + dest.getAbsolutePath());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,35 +1,52 @@
 package tests;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.ITestResult;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import pages.LoginPage;
 
+import utils.ScreenshotUtil;
+
 public class LoginTest {
+
+    WebDriver driver;
 
     @Test
     public void testLogin() {
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // for Jenkins
+        options.addArguments("--headless=new");
         options.addArguments("--disable-gpu");
 
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
 
         driver.get("https://practicetestautomation.com/practice-test-login/");
 
         LoginPage login = new LoginPage(driver);
         login.login("student", "Password123");
 
-        
         String currentUrl = driver.getCurrentUrl();
 
-        if (currentUrl.contains("logged-in-successfully")) {
-            System.out.println("Test Passed");
-        } else {
-            throw new RuntimeException("Test Failed");
+        if (!currentUrl.contains("logged-in-successfully")) {
+            throw new RuntimeException("Login Failed");
+        }
+    }
+
+    // 🔥 Screenshot logic here
+    @AfterMethod
+    public void captureResult(ITestResult result) {
+
+        String testName = result.getName();
+
+        if (result.getStatus() == ITestResult.FAILURE) {
+            ScreenshotUtil.capture(driver, testName + "_FAILED");
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            ScreenshotUtil.capture(driver, testName + "_PASSED");
         }
 
         driver.quit();
